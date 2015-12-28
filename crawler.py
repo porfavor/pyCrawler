9a05bedabe89ca591d98256ef890002c2567997e
porfavor <chris.hu.cohen@gmail.com>
2015-12-28 15:26:36 +0800

    Finish analyze lottery result html.

--- crawler.py
+++ crawler.py
@@ -4,25 +4,57 @@ import urllib
 import re
 
 
-def getHtml(url):
+def get_html(url):
     page = urllib.urlopen(url)
     html = page.read()
     return html
 
 
-# def getInfo(html):
+def get_record_fields(rec):
+    # 1. remove space and \r\n
+    pattern = re.compile(r'>\s*<', re.S)
+    sub_text = pattern.sub('><', rec)
+
+    # 2. search for fields in <td></td>
+    pattern = re.compile(
+            r'.*?<td.*?>(?P<date>.+?)</td>\s*?<td.*?>(?P<number>.+?)</td>\s*?<td.*?>(?P<balls>.+?)</td>\s*?<td.*?>(?P<prize2>.+?)</td>\s*?<td.*?>(?P<prize1>.+?)</td>.*',
+            re.I | re.M | re.S)
+    m = pattern.search(sub_text)
+
+    date = m.group('date')
+    number = m.group('number')
+    balls = m.group('balls')
+    prize1 = m.group('prize1')
+    prize2 = m.group('prize2')
+    #print date, number, balls, prize1, prize2
+
+    # 3. search for data in <em></em>
+    pattern = re.compile(
+            r'<em.*?>(?P<ball1>.+?)</em><em.*?>(?P<ball2>.+?)</em><em.*?>(?P<ball3>.+?)</em><em.*?>(?P<ball4>.+?)</em><em.*?>(?P<ball5>.+?)</em><em.*?>(?P<ball6>.+?)</em><em>(?P<ball7>.+?)</em>',
+            re.I | re.M | re.S)
+    # print pattern.groupindex
+    m2 = pattern.search(balls)
+    # print pattern.groups()
+    red = m2.group('ball1') + ',' + m2.group('ball2') + ',' + m2.group('ball3') + ',' + m2.group('ball4') + ',' + m2.group('ball5') + ',' + m2.group('ball6')
+    blue = m2.group('ball7')
+
+    pattern = re.compile(r'<strong>(?P<prize>.+?)</strong>.*', re.I | re.M | re.S)
+    m2 = pattern.search(prize1)
+    prize1 = m2.group('prize')
+
+    m2 = pattern.search(prize2)
+    prize2 = m2.group('prize')
+
+    result = {'date': date, 'number': number, 'red': red, 'blue': blue, 'prize1': prize1, 'prize2': prize2}
+    return result
 
 
-html = getHtml("http://kaijiang.zhcw.com/zhcw/html/ssq/list_1.html")
+# main start
+html = get_html("http://kaijiang.zhcw.com/zhcw/html/ssq/list_1.html")
 
-list = re.findall(r'<tr>.*?<em.*?</tr>', html, re.I | re.M | re.S)
+records = re.findall(r'<tr>.*?<em.*?</tr>', html, re.I | re.M | re.S)
 
 i = 0
-for val in list:
+for record in records:
     i += 1
-    # print str(i) + ':'
-    print val
-
-
-# pattern = re.compile(r'(<td.*?>?P<date></td>)\s+(<td.*?>?P<no></td>)\s+.*', re.I)
-# m=p.search(s)
+    print 'record ' + str(i) + ': ' + str(get_record_fields(record))
--- testCrawler.py
+++ testCrawler.py
@@ -35,9 +35,9 @@ html = """
         </tr>
 """
 
-#html.replace('\n', '')
+# html.replace('\n', '')
 
-dr = re.compile(r'>\s*<',re.S)
+dr = re.compile(r'>\s*<', re.S)
 dd = dr.sub('><', html)
 print dd
 # print html
@@ -56,15 +56,39 @@ print dd
 # p = re.compile(r'(?P<name>[a-z]+)\s+(?P<age>\d+)\s+(?P<tel>\d+).*', re.I)
 
 p = re.compile(
-    r'.*?<td.*?>(?P<date>.+?)</td>\s*?<td.*?>(?P<id>.+?)</td>\s*?<td.*?>(?P<balls>.+?)</td>\s*?<td.*?>(?P<prize1>.+?)</td>\s*?<td.*?>(?P<prize2>.+?)</td>.*',
-    re.I | re.M | re.S)
-print p.groupindex
+        r'.*?<td.*?>(?P<date>.+?)</td>\s*?<td.*?>(?P<id>.+?)</td>\s*?<td.*?>(?P<balls>.+?)</td>\s*?<td.*?>(?P<prize1>.+?)</td>\s*?<td.*?>(?P<prize2>.+?)</td>.*',
+        re.I | re.M | re.S)
+# print p.groupindex
 
-m = p.search(html)
-
+m = p.search(dd)
 # print m.groups()
-print 'date: ' + m.group('date')
-print 'id: ' + m.group('id')
-print 'balls: ' + m.group('balls')
-print 'prize1: ' + m.group('prize1')
-print 'prize2: ' + m.group('prize2')
+
+# balls = re.sub(r'>\s*<', '><', m.group('balls'), re.S)
+# balls = re.sub(r'\s*', '', balls, re.S)
+
+date = m.group('date')
+id = m.group('id')
+balls = m.group('balls')
+
+p2 = re.compile(
+    r'<em.*?>(?P<ball1>.+?)</em><em.*?>(?P<ball2>.+?)</em><em.*?>(?P<ball3>.+?)</em><em.*?>(?P<ball4>.+?)</em><em.*?>(?P<ball5>.+?)</em><em.*?>(?P<ball6>.+?)</em><em>(?P<ball7>.+?)</em>',
+    re.I)
+#print p2.groupindex
+m2 = p2.search(m.group('balls'))
+#print m2.groups()
+red = m2.group('ball1') + ',' + m2.group('ball2') + ',' + m2.group('ball3') + ',' + m2.group('ball4') + ',' + m2.group('ball5') + ',' + m2.group('ball6')
+blue = m2.group('ball7')
+
+p2 = re.compile(r'<strong>(?P<prize>.+)</strong>')
+m2 = p2.search(m.group('prize1'))
+prize1 = m2.group('prize')
+
+m2 = p2.search(m.group('prize2'))
+prize2 = m2.group('prize')
+
+print 'date: ' + date
+print 'id: ' + id
+print 'red: ' + red
+print 'blue: ' + blue
+print 'prize1: ' + prize1
+print 'prize2: ' + prize2
